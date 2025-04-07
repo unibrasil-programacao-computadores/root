59b0c919d6ca262f3406dee4d6820f9989c274cc
Thiago Nascimento <nascimenthiago@gmail.com>
2025-04-07 13:36:39 -0300

    Define tad matriz

--- matriz_esparsa/matriz.h
+++ matriz_esparsa/matriz.h
@@ -2,6 +2,7 @@
 #define MATRIZ_H_INCLUDED
 
 typedef struct celula_t *apontador;
+
 typedef struct celula_t {
     apontador direita;
     apontador abaixo;
@@ -9,5 +10,18 @@ typedef struct celula_t {
     int coluna;
     float valor;
 } celula;
+
+typedef struct {
+    int m_linhas;
+    int n_colunas;
+    apontador raiz;
+} matriz;
+
+void le_matriz        (matriz* mat);
+void apaga_matriz     (matriz* mat);
+void soma_matriz      (matriz* mat1, matriz* mat2, matriz* mat_soma);
+void multiplica_matriz(matriz* mat1, matriz* mat2, matriz* mat_produto);
+void imprime_matriz   (matriz* mat);
+void insere_na_matriz (int linha, int coluna, float valor, matriz* mat);
 
 #endif // MATRIZ_H_INCLUDED
