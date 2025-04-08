3e47a0e2cd86d9be2c4473f6ccc077da5700863e
Thiago Nascimento <nascimenthiago@gmail.com>
2025-04-08 14:45:43 -0300

    Add matriz esparsa teste

--- matriz_esparsa/matriz.c
+++ matriz_esparsa/matriz.c
@@ -1,1 +1,33 @@
 #include "matriz.h"
+
+void le_matriz(matriz *mat)
+{
+
+}
+
+void apaga_matriz(matriz *mat)
+{
+
+}
+
+void soma_matriz(const matriz *mat1, const matriz *mat2, matriz *mat_soma)
+{
+
+}
+
+void multiplica_matriz(const matriz *mat1, const matriz *mat2, matriz *mat_produto)
+{
+
+
+}
+
+void imprime_matriz(const matriz* mat)
+{
+
+
+}
+
+void insere_na_matriz (int linha, int coluna, float valor, matriz *mat)
+{
+
+}
--- matriz_esparsa/matriz.h
+++ matriz_esparsa/matriz.h
@@ -17,11 +17,11 @@ typedef struct {
     apontador raiz;
 } matriz;
 
-void le_matriz        (matriz* mat);
-void apaga_matriz     (matriz* mat);
-void soma_matriz      (matriz* mat1, matriz* mat2, matriz* mat_soma);
-void multiplica_matriz(matriz* mat1, matriz* mat2, matriz* mat_produto);
-void imprime_matriz   (matriz* mat);
-void insere_na_matriz (int linha, int coluna, float valor, matriz* mat);
+void le_matriz        (matriz *mat);
+void apaga_matriz     (matriz *mat);
+void soma_matriz      (const matriz *mat1, const matriz *mat2, matriz *mat_soma);
+void multiplica_matriz(const matriz *mat1, const matriz *mat2, matriz *mat_produto);
+void imprime_matriz   (const matriz* mat);
+void insere_na_matriz (int linha, int coluna, float valor, matriz *mat);
 
 #endif // MATRIZ_H_INCLUDED
